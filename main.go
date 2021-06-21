8530f558c2f208c2391412db7f019341ce3e589c
fenghaojiang <457420285@qq.com>
2021-06-21 22:32:59 +0800

    fix cron

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	options := cron.NewParser(cron.Second | cron.Minute |
 		cron.Hour | cron.Dom | cron.Month | cron.DowOptional | cron.Descriptor)
 	cronJob := cron.New(cron.WithParser(options), cron.WithChain())
-	_, err := cronJob.AddFunc("0 0/1 * * * *", action.GetStockInfo)
+	_, err := cronJob.AddFunc("0 0/1 9-15 * * *", action.GetStockInfo) //from 9:00AM to 15:00PM
 	if err != nil {
 		fmt.Println(err.Error())
 		return
